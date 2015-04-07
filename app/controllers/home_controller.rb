3d3c81cca1bfc4886c11ed9eb90e838716216e88
Mu-ik Jeon <ed.jeon@daumkakao.com>
2015-04-07 10:30:06 +0900

    design view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -27,13 +27,22 @@ class HomeController < ApplicationController
 
   private
   def get_response(s, m)
+    past_days = (Time.now.localtime.strftime('%Y%m%d').to_time -
+                 m.time.localtime.strftime('%Y%m%d').to_time) / 1.day
+    if past_days == 0
+      day = '오늘'
+    elsif past_days == 1
+      day = '어제'
+    else
+      day = "#{past_days}일 전"
+    end
     {
       station: {
         name: s.name
       },
       measure: {
         grade: m.grade,
-        time: m.time.localtime.to_s
+        time: m.time.localtime.strftime("#{day} %-H시")
       }
     }
   end
