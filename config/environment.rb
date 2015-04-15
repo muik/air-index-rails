9bb7bd3aab5f57dd3e6093227fc83f36d0ca1618
Mu-ik Jeon <muikor@gmail.com>
2015-04-15 14:22:47 +0900

    sidekiq의 worker에서 export한 환경변수를 읽어오지 못해 파일로 불러오기

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# sidekiq의 worker에서 export한 환경변수를 읽어오지 못해 파일로 불러오기
+if !ENV['MONGOID_USERNAME'] && Rails.env == 'production'
+  require File.expand_path('../../../../shared/env', __FILE__)
+end 
+
 # Initialize the Rails application.
 Rails.application.initialize!
