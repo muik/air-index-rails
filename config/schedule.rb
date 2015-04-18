68d4400bdc3aadb0db3657b2dd09059a7363a7ef
Mu-ik Jeon <muikor@gmail.com>
2015-04-18 19:55:07 +0900

    no shedule jobs on staging stage

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,13 @@
-every 1.day, :at => '12:30 pm' do
-  runner 'RegionCrawler::perform_async'
-end
+if @environment != 'staging'
+  every 1.day, :at => '12:30 pm' do
+    runner 'RegionCrawler::perform_async'
+  end
 
-every :hour do
-  runner 'MeasureCrawler::perform_all'
-end
+  every :hour do
+    runner 'MeasureCrawler::perform_all'
+  end
 
-every '0 5,11,17,23 * * *' do
-  runner 'ForecastCrawler::perform_async'
+  every '0 5,11,17,23 * * *' do
+    runner 'ForecastCrawler::perform_async'
+  end
 end
