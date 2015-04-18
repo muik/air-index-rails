df98a0e7c60340462dd99ce8d40284103cb3e42e
Mu-ik Jeon <muikor@gmail.com>
2015-04-18 18:43:04 +0900

    staging deploy

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # sidekiq의 worker에서 export한 환경변수를 읽어오지 못해 파일로 불러오기
-if !ENV['MONGOID_USERNAME'] && Rails.env == 'production'
+if !ENV['MONGOID_USERNAME'] && (Rails.env == 'production' or Rails.env == 'staging')
   require File.expand_path('../../../../shared/env', __FILE__)
 end 
 
