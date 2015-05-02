1c12d6713f63e083b4f30d9b11d76f0d8b26063d
Mu-ik Jeon <muikor@gmail.com>
2015-05-02 15:45:09 +0900

    admin login

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  if !Rails.env.production?
+  if Rails.env.production?
     http_basic_authenticate_with :name => ENV["ADMIN_USERNAME"], :password => ENV["ADMIN_PASSWORD"]
   end
 
