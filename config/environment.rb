ac615c216b35edff203bbe3bf0e0ff914aa2d4b4
Mu-ik Jeon <muikor@gmail.com>
2015-05-02 17:06:44 +0900

    admin login

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # sidekiq의 worker에서 export한 환경변수를 읽어오지 못해 파일로 불러오기
 if !ENV['MONGOID_USERNAME'] && (Rails.env == 'production' or Rails.env == 'staging')
-  require File.expand_path('../../../../shared/env', __FILE__)
+  begin
+    require File.expand_path('../../../../shared/env', __FILE__)
+  rescue Exception => e
+    require File.expand_path('../../../shared/env', __FILE__)
+  end
 end 
 
 # Initialize the Rails application.
